225ad3302e9655fa694fd8c9c8f2a4fdc97ae17d
john2k2 <ortiz.jonathan@live.com>
2022-11-23 23:53:19 -0300

    dia 1

--- src/components/MainHeader/NavLinkHeader.jsx
+++
@@ -1,9 +0,0 @@
-const NavLinkHeader = ({ text }) => {
-    return (
-        <div className="relative mb-11 text-2xl font-bold capitalize transition-all hover:text-Soft-red md:mb-0 ">
-            <a href="#">{text}</a>
-        </div>
-    );
-};
-
-export default NavLinkHeader;
